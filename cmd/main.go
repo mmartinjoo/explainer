65f929bf917c87791c98193f91891f4fab7e24ab
martinjoo <martin@martinjoo.dev>
2024-12-13 23:30:10 +0100

    Execute EXPLAIN queries

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/mmartinjoo/explainer/internal/parser"
+	"github.com/mmartinjoo/explainer/internal/runner"
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/analytics")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	queries, err := parser.Parse("/Users/joomartin/code/explainer/queries.log")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%#v\n", queries)
+
+	_, err = runner.Run(db, queries)
+	if err != nil {
+		panic(err)
+	}
 }
