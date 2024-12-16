9c87f30fb561ba5556f2ce489cdcb8e8862e8659
martinjoo <martin@martinjoo.dev>
2024-12-16 21:16:12 +0100

    Extract out functions

--- internal/tableanalyzer/analyzer.go
+++ internal/tableanalyzer/analyzer.go
@@ -87,24 +87,6 @@ func (r *Result) analyzeTooLongTextColumns(db *sql.DB, table string) error {
 	return nil
 }
 
-func findCompositeIndexes(indexes []Index) (CompositeIndexes, error) {
-	hmap := make(CompositeIndexes)
-	for _, idx := range indexes {
-		hmap[idx.keyName] = append(hmap[idx.keyName], idx)
-	}
-	for k, v := range hmap {
-		if len(v) == 1 {
-			delete(hmap, k)
-		}
-	}
-	for k := range hmap {
-		slices.SortFunc(hmap[k], func(a, b Index) int {
-			return int(a.seq) - int(b.seq)
-		})
-	}
-	return hmap, nil
-}
-
 func (r *Result) analyzeStringIndexes(db *sql.DB, table string) error {
 	stringCols, err := queryStringColumns(db, table)
 	if err != nil {
@@ -173,24 +155,6 @@ func (r *Result) analyzeCompositeIndexes(db *sql.DB, table string) error {
 		r.grade = grade.Dec(r.grade, 2)
 	}
 	return nil
-}
-
-// checkCardinality checks if columns in a composite index are ordered based on their cardinality
-// If it's not ordered well, the function returns the optimal index in the right order
-func checkCardinality(compIdx CompositeIndex) (optimalIndex CompositeIndex, ok bool) {
-	optimalIdx := make([]Index, len(compIdx))
-	copy(optimalIdx, compIdx)
-
-	slices.SortFunc(optimalIdx, func(a, b Index) int {
-		return int(a.cardinality) - int(b.cardinality)
-	})
-
-	for i, v := range optimalIdx {
-		if compIdx[i] != v {
-			return optimalIdx, false
-		}
-	}
-	return nil, true
 }
 
 func (r *Result) Grade() float32 {
---
+++ internal/tableanalyzer/index.go
@@ -0,0 +1,39 @@
+package tableanalyzer
+
+import "slices"
+
+func findCompositeIndexes(indexes []Index) (CompositeIndexes, error) {
+	hmap := make(CompositeIndexes)
+	for _, idx := range indexes {
+		hmap[idx.keyName] = append(hmap[idx.keyName], idx)
+	}
+	for k, v := range hmap {
+		if len(v) == 1 {
+			delete(hmap, k)
+		}
+	}
+	for k := range hmap {
+		slices.SortFunc(hmap[k], func(a, b Index) int {
+			return int(a.seq) - int(b.seq)
+		})
+	}
+	return hmap, nil
+}
+
+// checkCardinality checks if columns in a composite index are ordered based on their cardinality
+// If it's not ordered well, the function returns the optimal index in the right order
+func checkCardinality(compIdx CompositeIndex) (optimalIndex CompositeIndex, ok bool) {
+	optimalIdx := make([]Index, len(compIdx))
+	copy(optimalIdx, compIdx)
+
+	slices.SortFunc(optimalIdx, func(a, b Index) int {
+		return int(a.cardinality) - int(b.cardinality)
+	})
+
+	for i, v := range optimalIdx {
+		if compIdx[i] != v {
+			return optimalIdx, false
+		}
+	}
+	return nil, true
+}
