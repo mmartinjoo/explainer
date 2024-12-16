576afbf942053346c538e9c6f402e4a98029eea3
martinjoo <martin@martinjoo.dev>
2024-12-16 18:42:33 +0100

    Add flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/mmartinjoo/explainer/internal/explainer"
+	"github.com/mmartinjoo/explainer/internal/tableanalyzer"
 	"log"
 	"os"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/mmartinjoo/explainer/internal/tableanalyzer"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/analytics")
+	opts, err := newOptsFromOSArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("mysql", opts.connectionString())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,4 +49,47 @@ func main() {
 		fmt.Printf("Invalid argument: %s\n", os.Args[1])
 		os.Exit(1)
 	}
+}
+
+type Opts struct {
+	host string
+	port int
+	user string
+	pass string
+	db   string
+}
+
+func newOptsFromOSArgs() (Opts, error) {
+	host := flag.String("host", "localhost", "Host address")
+	db := flag.String("database", "", "Database")
+	port := flag.Int("port", 3306, "Host port")
+	user := flag.String("user", "root", "Username")
+	pass := flag.String("pass", "root", "Password")
+	flag.Parse()
+
+	if *host == "" || *db == "" || *port == 0 || *user == "" || *pass == "" {
+		return Opts{}, ValidationError{}
+	}
+
+	return Opts{
+		host: *host,
+		db:   *db,
+		port: *port,
+		user: *user,
+		pass: *pass,
+	}, nil
+}
+
+func (o Opts) connectionString() string {
+	// "root:root@tcp(127.0.0.1:3306)/analytics"
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", o.user, o.pass, o.host, o.port, o.db)
+}
+
+type ValidationError struct {
+	field string
+	value any
+}
+
+func (e ValidationError) Error() string {
+	return fmt.Sprintf("--host --port --database --user --pass cannot be empty")
 }
