98fef0c118f63293a741f2a75ff1b45243b664de
martinjoo <martin@martinjoo.dev>
2024-12-16 20:57:30 +0100

    Handle args and flags

--- cmd/main.go
+++ cmd/main.go
@@ -11,85 +11,83 @@ import (
 	"os"
 )
 
+const (
+	name    = "myexplainer"
+	version = "v0.0.1"
+)
+
+var (
+	host     *string
+	database *string
+	port     *int
+	user     *string
+	pass     *string
+)
+
 func main() {
-	opts, err := newOptsFromOSArgs()
-	if err != nil {
-		log.Fatal(err)
+	host = flag.String("host", "localhost", "Host address")
+	database = flag.String("database", "", "Database name")
+	port = flag.Int("port", 3306, "Host port")
+	user = flag.String("user", "root", "Username")
+	pass = flag.String("pass", "root", "Password")
+	help := flag.Bool("help", false, "Show help message")
+	ver := flag.Bool("version", false, "Show version")
+
+	flag.Parse()
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s %s\n", name, version)
+		fmt.Fprintf(os.Stderr, "==================\n")
+		fmt.Fprintf(os.Stderr, "A CLI tool for analyzing queries and DB tables. It is meant to be used in local environment not in production.\n\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(os.Stderr, "'myexplainer table <tablename>' analyzes the table structure and gives you performance-related warnings, if any\n")
+		fmt.Fprintf(os.Stderr, "'myexplainer logs <path>' reads a log file in which every line contains a SQL query and analyzes them using EXPLAIN and gives you detailed information and tips\n\n")
+		fmt.Fprintf(os.Stderr, "Examples:\n")
+		fmt.Fprintf(os.Stderr, "'myexplainer --database analytics table page_views' will analyze the 'page_views' table in the 'analytics' database on 'localhost' (default) with user 'root' (default) and password 'root' (default)\n\n")
+		fmt.Fprintf(os.Stderr, "'myexplainer --database analytics logs ./queries.log' will read the 'queries.log' file, parse the queries that it contains and then run EXPLAIN queries in the 'analytics' database on 'localhost' (default) with user 'root' (default) and password 'root' (default)\n\n")
+		fmt.Fprintf(os.Stderr, "Flags:\n")
+		flag.PrintDefaults()
 	}
 
-	db, err := sql.Open("mysql", opts.connectionString())
+	if *help {
+		flag.Usage()
+		return
+	}
+	if *ver {
+		fmt.Printf("%s %s\n", name, version)
+		return
+	}
+
+	db, err := sql.Open("mysql", connectionString())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	if len(os.Args) == 1 {
-		fmt.Printf("Usage:\nexplainer logs <path> to analyze a log file of SQL queries\nexplainer table <table> to analyze a table\n")
-		os.Exit(1)
+	if len(os.Args) < 2 {
+		flag.Usage()
+		return
 	}
 
-	switch os.Args[1] {
+	param := os.Args[len(os.Args)-1]
+	cmd := os.Args[len(os.Args)-2]
+
+	switch cmd {
 	case "logs":
-		if len(os.Args) != 3 {
-			fmt.Printf("Usage:\nexplainer logs <path> to analyze a log file of SQL queries\nexplainer table <table> to analyze a table\n")
-			os.Exit(1)
-		}
-		if err = explainer.Explain(db, os.Args[2]); err != nil {
+		if err = explainer.Explain(db, param); err != nil {
 			log.Fatal(err)
 		}
 	case "table":
-		if len(os.Args) != 3 {
-			fmt.Printf("Usage:\nexplainer logs <path> to analyze a log file of SQL queries\nexplainer table <table> to analyze a table\n")
-			os.Exit(1)
-		}
-		if err = tableanalyzer.Analyze(db, os.Args[2]); err != nil {
+		if err = tableanalyzer.Analyze(db, param); err != nil {
 			log.Fatal(err)
 		}
 	default:
-		fmt.Printf("Invalid argument: %s\n", os.Args[1])
-		os.Exit(1)
+		flag.Usage()
+		return
 	}
 }
 
-type Opts struct {
-	host string
-	port int
-	user string
-	pass string
-	db   string
-}
-
-func newOptsFromOSArgs() (Opts, error) {
-	host := flag.String("host", "localhost", "Host address")
-	db := flag.String("database", "", "Database")
-	port := flag.Int("port", 3306, "Host port")
-	user := flag.String("user", "root", "Username")
-	pass := flag.String("pass", "root", "Password")
-	flag.Parse()
-
-	if *host == "" || *db == "" || *port == 0 || *user == "" || *pass == "" {
-		return Opts{}, ValidationError{}
-	}
-
-	return Opts{
-		host: *host,
-		db:   *db,
-		port: *port,
-		user: *user,
-		pass: *pass,
-	}, nil
-}
-
-func (o Opts) connectionString() string {
+func connectionString() string {
 	// "root:root@tcp(127.0.0.1:3306)/analytics"
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", o.user, o.pass, o.host, o.port, o.db)
-}
-
-type ValidationError struct {
-	field string
-	value any
-}
-
-func (e ValidationError) Error() string {
-	return fmt.Sprintf("--host --port --database --user --pass cannot be empty")
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", *user, *pass, *host, *port, *database)
 }
